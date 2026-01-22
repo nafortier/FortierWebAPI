d6b3db4b0052ca3c679c6030770df0dd76e0728e
nafortier <147059443+nafortier@users.noreply.github.com>
2026-01-22 11:22:14 -0500

    we

--- public/js/edit.js
+++ public/js/edit.js
@@ -6,13 +6,13 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
 async function loadScore(){
-    const res = await fetch(`/api/highscores/${encodedURIComponent(id)}`)
+    const res = await fetch(`/api/highscores/${encodeURIComponent(id)}`)
 
     const score = await res.json();
 
-    document.getElementById("playername".value = score.playername ?? "");
-    document.getElementById("score".value = score.score ?? 0);
-    document.getElementById("level".value = score.level ?? "");
+    document.getElementById("playername").value = score.playername ?? "";
+    document.getElementById("score").value = score.score ?? 0;
+    document.getElementById("level").value = score.level ?? "";
 }
 
 
@@ -23,7 +23,7 @@ form.addEventListener("submit", async (e)=>{
     const score = Number(document.getElementById("score").value);
     const level = Number(document.getElementById("level").value);
 
-    const res = await fetch(`/api/highscores/${encodedURIComponent(id)}`, {
+    const res = await fetch(`/api/highscores/${encodeURIComponent(id)}`, {
         method:"PUT",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({playername, score, level}),
